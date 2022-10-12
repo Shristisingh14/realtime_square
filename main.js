2a9ce93f1a5fa1af4cf3e98f549eb2252bb75d83
Shristisingh14 <84385618+Shristisingh14@users.noreply.github.com>
2022-10-12 16:28:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,52 +1,30 @@
-x = 0;
-y = 0;
-draw_circle = "";
-draw_rectangle = "";
+noseX=0;
+noseY=0;
+difference=0;
+rightwristX = 0;
+leftwristX = 0;
 
 function setup() {
-    canvas = createCanvas(900, 600);
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(600, 500);
+    canvas.position(600, 140);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
-var SpeechRecognition = window.webkitSpeechRecognition;
-
-var recognition = new SpeechRecognition();
-
-function start() {
-    document.getElementById("status").innerHTML = "System is listening please speak";
-    recognition.start();
+function modelLoaded() {
+    console.log('PoseNet is Initialized');
 }
 
-recognition.onresult = function(event) {
-    console.log(event);
-    var content = event.results[0][0].transcript;
-
-    document.getElementById("status").innerHTML = "The Speech has been recognised as: " + content;
-     if(content == "Circle."){
-        x = Math.floor(Math.random() * 900);
-        y = Math.floor(Math.random() * 600);
-        document.getElementById("status").innerHTML = "Started drawing circle";
-        draw_circle = "set";
-     }
-     if(content == "Rectangle."){
-        x = Math.floor(Math.random() * 900);
-        y = Math.floor(Math.random() * 600);
-        document.getElementById("status").innerHTML = "Started drawing rectangle";
-        draw_rectangle = "set";
-     }
+function gotPoses(results)
+{
+    if(results.length > 0 )
+    {
+        console.log(results);
+    }
 }
 
 function draw() {
-    if(draw_circle == "set")
-    {
-        radius = Math.floor(Math.random() * 100);
-        circle(x, y, radius);
-        document.getElementById("status").innerHTML = "Circle is drawn.";
-        draw_circle = "";
-    }
-    if(draw_rectangle == "set")
-    {
-        rect(x, y, 75, 50);
-        document.getElementById("status").innerHTML = "Rectangle is drawn.";
-        draw_rectangle = "";
-    }
+    background('#808080');
 }
