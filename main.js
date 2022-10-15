36d31aa675bdc571504c994eac55712f44fe4b16
Shristisingh14 <84385618+Shristisingh14@users.noreply.github.com>
2022-10-15 16:24:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -22,9 +22,22 @@ function gotPoses(results)
     if(results.length > 0 )
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + " noseY = " + noseY);
+        leftwristX = results[0].pose.leftWrist.x;
+        rightwristX = results[0].pose.rightWrist.x;
+        difference = floor(leftwristX - rightwristX);
+        console.log("leftWristX = " + leftwristX);
+        console.log("rightWristX = " + rightwristX);
+        console.log("difference = " + difference);
     }
 }
 
 function draw() {
     background('#808080');
+    document.getElementById("square_side").innerHTML = "Width and height of the square will be = " +difference +"px";
+    fill('#3F00FF');
+    stroke('#1434A4');
+    square(noseX, noseY, difference);
 }
